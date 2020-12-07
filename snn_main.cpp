5eff9031ef1efb3572937808fea688920064eafb
Jose E. Aguilar Escamilla <aguilar.jose11@outlook.com>
2020-12-07 00:34:25 -0600

    Add tests checking euclidean and weight functions

--- snn_main.cpp
+++ snn_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <armadillo>
 #include <vector>
+#include <cstdlib>
 #include "ou_snn.h"
 
 /**
@@ -14,6 +15,13 @@ arma::Col<double> get_initial_delays();
 arma::Col<double> get_initial_weights();
 
 int main(int argc, const char **argv) {
+    
+
+
+
+
+
+
     // Test individual LIF/SRM neuron
     int snn_id = 2;
     int n_inputs = 2;
@@ -28,7 +36,7 @@ int main(int argc, const char **argv) {
     double round_zero = 0.1;
 
     OU_LIF_SRM neuron(snn_id, n_inputs, n_lateral, init_d, init_w, tau_m, 
-    u_rest, init_v, t_rest, kappa_naugh, round_zero);
+    u_rest, init_v, t_rest, kappa_naugh, round_zero, 0, 0);
 
     printf("delay vector: [");
     for(unsigned int i = 0; i < neuron.d_j.size(); i++)
@@ -157,22 +165,28 @@ int main(int argc, const char **argv) {
     // Testing neural network
     // parameters:
     unsigned int i_layer_size = 2;
-    unsigned int h_layer_size = 10;
+    unsigned int h_layer_size = 16;
     std::vector<arma::Col<double>> d_init({
-        {4, 3, 2, 3, 4, 3, 3, 2, 1, 4}, 
-        {1, 4, 2, 4, 6, 2, 1, 3, 5, 3}
+        {4, 3, 2, 3, 4, 3, 3, 2, 1, 4, 3, 3, 3, 2, 5, 2}, 
+        {1, 4, 2, 4, 6, 2, 1, 3, 5, 3, 3, 3, 3, 2, 5, 2}
     });
     std::vector<arma::Col<double>> w_init({
-        {1, 3, 2, -1, 3, 1, -2, 1, -2, 1},
-        {2, 4, 2, 3, 4, 12, 1, 1, 2, 1},
-        {2, 3, 2, 1, 2, 1, 2, 1, 1, 2},
-        {-1, -2, -1, -2, -3, -4, -9, -1, 2, -2},
-        {2, 4, 2, 1, 3, 6, 3, 1, 2, 3},
-        {1, 3, 1, 3, 5, 3, 1, 0, 3, 1},
-        {-3, -3, -1, -3, -5, 3, 2, 4, 5, 4},
-        {4, 6, 2, 2, 4, 6, 3, 2, 4, 5},
-        {-3, -5, -1, 4, -1, -3, 2, -7, -1, 3},
-        {2, 5, 7, 2, 4, 6, 4, 1, 3, 5},
+        {1, 3, 2, -1, 3, 1, -2, 1, -2, 1, 3, 3, 3, 2, 5, 2},
+        {3, 4, 2, 3, 4, 12, 1, 1, 2, 1, 3, 3, 3, 2, 5, 2},
+        {2, 3, 2, 1, 2, 1, 2, 1, 1, 2, 3, 3, 3, 2, 5, 2},
+        {-1, -2, -1, -2, -3, -4, -9, -1, 2, -2, 3, 3, 3, 2, 5, 2},
+        {3, 4, 2, 1, 3, 6, 3, 1, 2, 3, 3, 3, 3, 2, 5, 2},
+        {1, 3, 1, 3, 5, 3, 1, 0, 3, 1, 3, 3, 3, 2, 5, 2},
+        {-2, -3, -1, -3, -5, 3, 2, 4, 5, 4, 3, 3, 3, 2, 5, 2},
+        {1, 6, 2, 2, 4, 6, 3, 2, 4, 5, 3, 3, 3, 2, 5, 2},
+        {-2, -5, -1, 4, -1, -3, 2, -7, -1, 3, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
+        {1, 5, 7, 2, 4, 6, 4, 1, 3, 5, 3, 3, 3, 2, 5, 2},
     });
     tau_m = 4;
     u_rest = 0;
@@ -183,7 +197,7 @@ int main(int argc, const char **argv) {
     alpha = 2;
 
     OU_SRM_NET snn(i_layer_size, h_layer_size, d_init, w_init,
-    tau_m, u_rest, init_v, t_reset, k_nought, round_zero, alpha);
+    tau_m, u_rest, init_v, t_reset, k_nought, round_zero, alpha, 4, 4, 1);
 
     for(int p = 0; p < 20; p++)
     {
@@ -196,12 +210,67 @@ int main(int argc, const char **argv) {
             {
                 // this neuron is the "winner"
                 printf("Spike %u winner (Reached threshold)\n", i);
-                return 0;
+                goto next_test;
             }
         }
     }
+    next_test:
+    // Test euclidean_distance_matrix function
+    double distance_unit = 2;
+
+    printf("Testing matrices.\n");
+    arma::Mat<double> e_mat = euclidean_distance_matrix (&snn, distance_unit);
+    printf("Matrix Content\n\n");
+    for(unsigned int i = 0; i < snn.h_size; i++)
+    {
+        for(unsigned int j = 0; j < snn.h_size; j++)
+        {
+            printf("%f\t", e_mat(i, j));
+        }
+        printf("\n");
+    }
 
 
+    // Test euclidean initial weight function
+    double sigma_1 = .5;
+    double sigma_2 = 2;
+    std::vector<arma::Col<double>> w_mat = initial_weight_euclidean(e_mat, sigma_1, sigma_2);
+    printf("\nPrinting weight matrix's contents:\n\n");
+    printf("Weight Matrix Content\n\n");
+    for(unsigned int i = 0; i < snn.h_size; i++)
+    {
+        for(unsigned int j = 0; j < snn.h_size; j++)
+        {
+            printf("%f\t", w_mat.at(i).at(j));
+        }
+        printf("\n");
+    }
 
+ 
+    // Testing euclidean_distance_matrix without snn
+    std::vector<std::vector<double>> point_list({
+        {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3},
+        {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3}
+    });
+
+    
+
+    printf("Testing matrices.\n");
+    arma::Mat<double> e_mat_init = euclidean_distance_matrix(&point_list, distance_unit);
+    printf("Matrix Content\n\n");
+    for(unsigned int i = 0; i < e_mat_init.n_rows; i++)
+    {
+        for(unsigned int j = 0; j < e_mat_init.n_cols; j++)
+        {
+            printf("%f\t", e_mat_init(i, j));
+        }
+        printf("\n");
+    }
+
+    // Armadillo version printout
+    arma::arma_version ver;
+    printf("Armadillo Version: %s\n", ver.as_string().c_str());
     return 0;
 }
+
+
